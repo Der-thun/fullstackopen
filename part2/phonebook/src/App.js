99d35df9c61c4b4177db79a8855917ab3f4f207a
Boris Kortunov <boris.kortunov@gmail.com>
2022-11-10 19:45:22 +0200

    feat: 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
-  const [newPhoneNumber, setPhoneNumber] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -16,11 +20,12 @@ const App = () => {
     if(inPhonebook.includes(addNewName) === false) {
       const nameObj = {
         name: addNewName,
-        phone: newPhoneNumber,
+        number: newPhoneNumber,
+        id: persons.length + 1,
       }
       setPersons(persons.concat(nameObj))
       setNewName('')
-      setPhoneNumber('')
+      setNewPhoneNumber('')
     } else {
       alert(`${addNewName} is already added to phonebook`)
     }    
@@ -31,12 +36,21 @@ const App = () => {
   }
 
   const handlePhoneAdd = (event) => {
-    setPhoneNumber(event.target.value)
+    setNewPhoneNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setNewFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value={newFilter} onChange={handleFilter}/>
+      </div>
+      
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameAdd}/>
@@ -50,7 +64,12 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.phone}</p>)}
+        {persons.map(person => {
+          if (person.name.toLocaleLowerCase().includes(newFilter.toLocaleLowerCase())) 
+            return <p key={person.id}>{person.name} {person.number}</p>
+          return ''
+          })
+          }
       </div>
     </div>
   )
